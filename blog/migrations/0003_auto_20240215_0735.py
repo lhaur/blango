5edd47bf6fac88697ac8bbba94aa08578de024f3
Lassi Hauru <lassi.hauru@metropolia.fi>
2024-02-15 10:35:17 +0000

    Finish crispy forms

--- blog/migrations/0003_auto_20240215_0735.py
+++ blog/migrations/0003_auto_20240215_0735.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='comment',
             name='content_type',
-            field=models.ForeignKey(default='lol', on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
+            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to='contenttypes.contenttype'),
             preserve_default=False,
         ),
         migrations.AddField(
