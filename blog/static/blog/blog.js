cf147742e428b4b7a95645b60aefdd4ba2afb14b
Lassi Hauru <lassi.hauru@metropolia.fi>
2024-03-04 11:52:44 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,18 @@
-alert('Hello, world!')
+function sayHello(yourName) {
+  if (yourName === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + yourName)
+  }
+}
+
+const yourName = 'Your Name'  // Put your name here
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(yourName)
+  }, 2000
+)
+
+console.log('After setTimeout')
